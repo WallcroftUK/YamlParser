3066bc1b52d858e334a243eced5651904f96a96c
Fizo55 <hugomacees56000@gmail.com>
2023-02-12 20:22:54 +0100

    cleanup

--- srcs/YamlParser/Extensions/StringExtensions.cs
+++ srcs/YamlParser/Extensions/StringExtensions.cs
@@ -1,21 +1,11 @@
-﻿namespace YamlParser.Extensions
+namespace YamlParser.Extensions
 {
     public static class StringExtensions
     {
         public static List<string> FilterLines(this string path, string[] startWith)
         {
             return File.ReadLines(path)
-                .Where(line =>
-                {
-                    foreach (var str in startWith)
-                    {
-                        if (line.StartsWith(str))
-                        {
-                            return true;
-                        }
-                    }
-                    return false;
-                })
+                .Where(line => startWith.Any(str => line.StartsWith(str)))
                 .ToList();
         }
     }
